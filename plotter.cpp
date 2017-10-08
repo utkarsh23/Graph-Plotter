f443ddc71d755f11b46b4ca3c555c8cd6b5c4651
Utkarsh Patil <utkarsh2305@gmail.com>
2017-10-08 12:13:36 +0530

    Update #2 and add arrowkey events

--- plotter.cpp
+++ plotter.cpp
@@ -1,62 +1,26 @@
 #include <string.h>
 #include <GL/glut.h>
+#include <vector>
 #include <math.h>
 #include <stdio.h>
 
-#define segments 1000000
+using namespace std;
+
+#define segments 100000
 int w = 1366, h = 768;
 int mouseX, mouseY;
+int functionType;
 
 const float segmentlen = 1.0 / segments;
 const float screenxstart = -5.0f, screenxstop = 5.0f;
 const float screenystart = -2.75f, screenystop = 2.75f;
 //Screen ranges from -5 to +5 on OpenGl coordinates
 
-float funcdata[20] = { 0 }, y[segments] = { 0 };
-int degree, funcoption;
+vector<double> funcdata;
+GLfloat y[segments] = { 0 };
+int degree;
 float startx, stopx; //Range of x to be plotted
 float starty = INFINITY, stopy = -INFINITY;
-
-float funcval(float x) {
-	int i;
-	float val = 0;
-	switch (funcoption) {
-		case 0:
-			for (i = 0; i <= degree; i++) {
-				val += funcdata[i] * pow(x, i);
-			}
-			return val;
-		case 1:
-			return sin(x);
-		case 2:
-			return cos(x);
-		case 3:
-			return tan(x);
-		case 4:
-			return 1.0 / sin(x);
-		case 5:
-			return 1.0 / cos(x);
-		case 6:
-			return 1.0  / tan(x);
-		default:
-			return val;
-	}
-}
-
-void precompute() {
-	int i;
-	float x = startx;
-	for (i = 0; i<segments; i++) {
-		y[i] = funcval(x);
-		if (y[i]<starty) {
-			starty = y[i];
-		}
-		if (y[i]>stopy) {
-			stopy = y[i];
-		}
-		x += (stopx - startx) / segments;
-	}
-}
 
 void dispString(double x, double y, char *string)
 {
@@ -65,21 +29,82 @@ void dispString(double x, double y, char *string)
 		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, string[i]);
 }
 
+GLfloat polynomialFunc(float x) {
+	int i;
+	float val = 0;
+	for (i = 0; i <= degree; i++) {
+		val += funcdata[i] * pow(x, i);
+	}
+	return val;
+}
+
+GLfloat operation(double val) {
+	switch (functionType) {
+		case 1:
+			return (GLfloat) sin(val);
+		case 2:
+			return (GLfloat) cos(val);
+		case 3:
+			return (GLfloat) tan(val);
+		case 4:
+			return (GLfloat) 1.0 / sin(val);
+		case 5:
+			return (GLfloat) 1.0 / cos(val);
+		case 6:
+			return (GLfloat) 1.0 / tan(val);
+		case 9:
+			return (GLfloat) polynomialFunc(val);
+		default:
+            return 0;
+			break;
+	}
+}
+
+
 void inpfunc() {
 	int i;
-	printf("0 -> polynomial function\n1 -> sin\n2 -> cos\n3 -> tan\n4 -> cosec\n5 -> sec\n6 -> cot\nEnter: ");
-	scanf("%d", &funcoption);
-	if (funcoption == 0) {
-		printf("Enter degree of polynomial.\n");
-		scanf("%d", &degree);
-		for (i = degree; i>0; i--) {
-			printf("Enter coefficient of x^%d.: ", i);
-			scanf("%f", &funcdata[i]);
-		}
-		printf("Enter value of constant term.: ");
-		scanf("%f", &funcdata[0]);
+	printf("Enter degree of polynomial.\n");
+	scanf("%d", &degree);
+	for (i = degree; i>0; i--) {
+		printf("Enter coefficient of x^%d.: ", i);
+		scanf("%lf", &funcdata[i]);
 	}
+	printf("Enter value of constant term.: ");
+	scanf("%lf", &funcdata[0]);
 	printf("Enter range of x in form [start] [stop] (0 0 for default): ");
+	scanf("%f %f", &startx, &stopx);
+	if (startx >= stopx) {
+		startx = -5;
+		stopx = 5;
+	}
+}
+
+void functionInput() {
+	printf("Trigonometric Functions:\n");
+	printf("Enter 1,2,3,4,5,6 for sine, cosine, tangent, cosecant, secant, cotangent\n");
+	printf("Polynomial functions:\n");
+	printf("Enter 9\n");
+	while (1) {
+		printf("Enter your choice: ");
+		scanf("%d", &functionType);
+		if (((functionType > 0) && (functionType < 7)) || (functionType == 9)) {
+			break;
+		}
+		else {
+			printf("Wrong choice!\n");
+		}
+	}
+	if (functionType == 9) {
+		printf("Enter degree of the polynomial: ");
+		scanf("%d", &degree);
+		double element;
+		for (int s = 0; s <= degree; s++) {
+			printf("Enter coefficient of term with degree %d: ", s);
+			scanf("%lf",  &element);
+			funcdata.push_back(element);
+		}
+	}
+    printf("Enter range of x in form [start] [stop] (0 0 for default): ");
 	scanf("%f %f", &startx, &stopx);
 	if (startx >= stopx) {
 		startx = -5;
@@ -95,8 +120,8 @@ void initRendering() {
 
 void handleKeypress(unsigned char key, int x, int y) {
 	switch (key) {
-		case 27:
-			exit(0);
+	case 27:
+		exit(0);
 	}
 }
 
@@ -110,6 +135,46 @@ void handleResize(int w, int h) {
 void mouseMotion(int x, int y) {
 	mouseX = x;
 	mouseY = y;
+}
+
+void precompute() {
+	int i;
+	float x = startx;
+	for (i = 0; i<segments; i++) {
+		y[i] = operation(x);
+		if (y[i]<starty) {
+			starty = y[i];
+		}
+		if (y[i]>stopy) {
+			stopy = y[i];
+		}
+		x += (stopx - startx) / segments;
+	}
+}
+
+void handleArrowpress(int key, int x, int y) {
+	switch (key) {
+		case GLUT_KEY_UP:
+			startx -= 10;
+			stopx += 10;
+			precompute();
+			break;
+		case GLUT_KEY_DOWN:
+			startx += 10;
+			stopx -= 10;
+			precompute();
+			break;
+		case GLUT_KEY_LEFT:
+			startx -= 10;
+			stopx -= 10;
+			precompute();
+			break;
+		case GLUT_KEY_RIGHT:
+			startx += 10;
+			stopx += 10;
+			precompute();
+			break;		
+	}
 }
 
 void drawPointLoc() {
@@ -137,7 +202,6 @@ void drawPointLoc() {
 	glEnd();
 	glPopMatrix();
 }
-
 void drawArrowAxes() {
 	int left = 1, right = 1, top = 1, bottom = 1;
 
@@ -222,7 +286,7 @@ void drawScene() {
 	glBegin(GL_LINE_STRIP);
 	int i;
 	float x = startx; //Actual value of x of function.
-	for (i = 0; i < segments; i++) {
+	for (i = 0; i<segments; i++) {
 		float xdisp = screenxstart + 10 * (x - startx) / (stopx - startx); //Corresponding value of x on screen.
 		float ydisp = screenystart + 5.5*(y[i] - starty) / (stopy - starty);
 		glVertex3f(xdisp, ydisp, 0.0f);
@@ -231,18 +295,20 @@ void drawScene() {
 	glEnd();
 
 	drawPointLoc();
+    
+    glutPostRedisplay();
 
 	glutSwapBuffers();
 }
 
 void update(int value) {
-	glutPostRedisplay();
-	glutTimerFunc(50, update, 0);
+	
+	glutTimerFunc(166, update, 0);
 }
 
 int main(int argc, char** argv) {
 
-	inpfunc();
+	functionInput();
 	precompute();
 
 	glutInit(&argc, argv);
@@ -257,8 +323,8 @@ int main(int argc, char** argv) {
 	glutPassiveMotionFunc(mouseMotion);
 	glutDisplayFunc(drawScene);
 	glutKeyboardFunc(handleKeypress);
+	glutSpecialFunc(handleArrowpress);
 	glutReshapeFunc(handleResize);
-	glutTimerFunc(50, update, 0);
 
 	glutMainLoop();
 
